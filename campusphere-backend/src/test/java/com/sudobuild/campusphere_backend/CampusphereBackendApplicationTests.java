f8cf4c7e9b6bbb417e243c7e8eb651f597bc2d66
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2026-04-04 14:50:46 +0300

    added the initial code of the backend

---
+++ campusphere-backend/src/test/java/com/sudobuild/campusphere_backend/CampusphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudobuild.campusphere_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
