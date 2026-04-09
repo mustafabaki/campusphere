ab12bc1509cf7ab119151e2db78231c14b40fd78
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2026-04-09 14:53:17 +0300

    add SocialMediaPlatform enum to represent various social media platforms

---
+++ campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/student_module/enums/SocialMediaPlatform.java
@@ -0,0 +1,16 @@
+package com.sudobuild.campusphere_backend.student_module.enums;
+
+public enum SocialMediaPlatform {
+    INSTAGRAM,
+    X,
+    FACEBOOK,
+    LINKEDIN,
+    GITHUB,
+    TIKTOK,
+    SNAPCHAT,
+    YOUTUBE,
+    DISCORD,
+    TELEGRAM,
+    REDDIT,
+    TWITCH
+}
