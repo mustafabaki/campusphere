20e67b74a6aeaa0f3df78514160834b021eb5569
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2026-04-15 14:08:00 +0300

    added the initial version of StudentServiceImpl.java

---
+++ campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/student_module/services/implementations/StudentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.sudobuild.campusphere_backend.student_module.services.implementations;
+
+import com.sudobuild.campusphere_backend.student_module.repositories.SocialLinkRepository;
+import com.sudobuild.campusphere_backend.student_module.repositories.StudentClubRepository;
+import com.sudobuild.campusphere_backend.student_module.repositories.StudentRepository;
+import com.sudobuild.campusphere_backend.student_module.services.StudentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    private final StudentRepository studentRepository;
+    private final StudentClubRepository studentClubRepository;
+    private final SocialLinkRepository socialLinkRepository;
+
+
+    public StudentServiceImpl(StudentRepository studentRepository, StudentClubRepository studentClubRepository, SocialLinkRepository socialLinkRepository) {
+        this.studentRepository = studentRepository;
+        this.studentClubRepository = studentClubRepository;
+        this.socialLinkRepository = socialLinkRepository;
+    }
+
+
+
+}
