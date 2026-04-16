fc25f864a310feba0a0159c46490de1f0f62954d
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2026-04-16 15:24:26 +0300

    add StudentCreateDTO, StudentResponseDTO, and StudentMapper for student data handling

---
+++ campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/student_module/mappers/StudentMapper.java
@@ -0,0 +1,33 @@
+package com.sudobuild.campusphere_backend.student_module.mappers;
+
+import com.sudobuild.campusphere_backend.student_module.DTOs.StudentCreateDTO;
+import com.sudobuild.campusphere_backend.student_module.DTOs.StudentResponseDTO;
+import com.sudobuild.campusphere_backend.student_module.models.Student;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentMapper {
+    public Student toStudent(@NotNull StudentCreateDTO studentDTO) {
+        Student student = new Student();
+        student.setId(studentDTO.getId());
+        student.setName(studentDTO.getName());
+        student.setSurname(studentDTO.getSurname());
+        student.setEmail(studentDTO.getEmail());
+        student.setPhone(studentDTO.getPhone());
+        return student;
+    }
+
+    public StudentResponseDTO toStudentResponseDTO(@NotNull Student student){
+        StudentResponseDTO studentResponseDTO = new StudentResponseDTO();
+        studentResponseDTO.setId(student.getId());
+        studentResponseDTO.setName(student.getName());
+        studentResponseDTO.setSurname(student.getSurname());
+        studentResponseDTO.setEmail(student.getEmail());
+        studentResponseDTO.setPhone(student.getPhone());
+        studentResponseDTO.setProfilePictureURL(student.getProfilePictureURL());
+        studentResponseDTO.setEnrolledClubs(student.getEnrolledClubs());
+        studentResponseDTO.setSocialLinks(student.getSocialLinks());
+        return studentResponseDTO;
+    }
+}
