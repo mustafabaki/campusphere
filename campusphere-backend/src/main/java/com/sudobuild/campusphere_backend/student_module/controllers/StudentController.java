953b324b1741d1300ac90293fb0d255a59f6bd32
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2026-04-20 15:55:21 +0300

    added an endpoint to fetch a student by email

--- campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/student_module/controllers/StudentController.java
+++ campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/student_module/controllers/StudentController.java
@@ -2,11 +2,10 @@ package com.sudobuild.campusphere_backend.student_module.controllers;
 
 import com.sudobuild.campusphere_backend.auxiliary.ApiResponse;
 import com.sudobuild.campusphere_backend.student_module.DTOs.StudentCreateDTO;
+import com.sudobuild.campusphere_backend.student_module.DTOs.StudentResponseDTO;
 import com.sudobuild.campusphere_backend.student_module.services.StudentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/student")
@@ -24,6 +23,18 @@ public class StudentController {
             return ApiResponse.success(savedStudent);
         } catch (Exception e) {
             return ApiResponse.failure(e.getMessage());
+        }
+    }
+
+    @GetMapping("/getStudentByEmail")
+    public ResponseEntity<ApiResponse> getStudentByEmail(@RequestParam("email") String email) {
+        try {
+            var savedStudent = studentService.getStudentByEmail(email);
+            ApiResponse<StudentResponseDTO> response = ApiResponse.success(savedStudent);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            var response = ApiResponse.failure(e.getMessage());
+            return ResponseEntity.badRequest().body(response);
         }
     }
 }
