9ddc83cfaf2b24fe2208977c45868641efedc503
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2026-07-14 11:26:17 +0300

    feat: add findByStatus to registration repository

--- campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/event_module/repositories/EventRegistrationRepository.java
+++ campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/event_module/repositories/EventRegistrationRepository.java
@@ -1,7 +1,11 @@
 package com.sudobuild.campusphere_backend.event_module.repositories;
 
+import com.sudobuild.campusphere_backend.event_module.enums.EventRegistrationStatus;
 import com.sudobuild.campusphere_backend.event_module.models.EventRegistration;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRegistrationRepository extends JpaRepository<EventRegistration, String> {
+    Slice<EventRegistration> findByStatus(EventRegistrationStatus status, Pageable pageable);
 }
