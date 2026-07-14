38e55eb83eb8b030d6e2f8b5bd7235d679b08b2f
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2026-07-14 11:27:11 +0300

    feat: add paginated event query methods

--- campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/event_module/repositories/EventRepository.java
+++ campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/event_module/repositories/EventRepository.java
@@ -1,9 +1,18 @@
 package com.sudobuild.campusphere_backend.event_module.repositories;
 
+import com.sudobuild.campusphere_backend.event_module.enums.EventStatus;
+import com.sudobuild.campusphere_backend.student_module.enums.Department;
+import com.sudobuild.campusphere_backend.student_module.models.StudentClub;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sudobuild.campusphere_backend.event_module.models.Event;
 
 public interface EventRepository extends JpaRepository<Event, String> {
-    
+    Slice<Event> findByDepartmentOrganizer(Department departmentOrganizer, Pageable pageable);
+    Slice<Event> findByStatus(EventStatus status, Pageable pageable);
+
+    Slice<Event> findByStudentClubOrganizer(StudentClub studentClubOrganizer, Pageable pageable);
 }
