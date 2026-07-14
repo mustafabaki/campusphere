908b41d14f15c6909a70924e36f956f78b94c37e
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2026-07-14 11:38:46 +0300

    feat: implement event registration and attendance management

--- campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/event_module/services/implementations/EventServiceImpl.java
+++ campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/event_module/services/implementations/EventServiceImpl.java
@@ -1,12 +1,13 @@
 package com.sudobuild.campusphere_backend.event_module.services.implementations;
 
+import com.sudobuild.campusphere_backend.event_module.enums.EventRegistrationStatus;
 import com.sudobuild.campusphere_backend.event_module.enums.EventStatus;
 import com.sudobuild.campusphere_backend.event_module.models.Event;
 import com.sudobuild.campusphere_backend.event_module.models.EventRegistration;
+import com.sudobuild.campusphere_backend.event_module.repositories.EventRegistrationRepository;
 import com.sudobuild.campusphere_backend.event_module.repositories.EventRepository;
 import com.sudobuild.campusphere_backend.event_module.services.EventService;
 import com.sudobuild.campusphere_backend.student_module.enums.Department;
-import com.sudobuild.campusphere_backend.student_module.models.Student;
 import com.sudobuild.campusphere_backend.student_module.repositories.StudentClubRepository;
 import com.sudobuild.campusphere_backend.student_module.repositories.StudentRepository;
 import org.springframework.data.domain.PageRequest;
@@ -14,17 +15,21 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 @Service
 public class EventServiceImpl implements EventService {
     private final EventRepository eventRepository;
     private final StudentClubRepository studentClubRepository;
     private final StudentRepository studentRepository;
+    private final EventRegistrationRepository eventRegistrationRepository;
 
 
-    public EventServiceImpl(EventRepository eventRepository, StudentClubRepository studentClubRepository, StudentRepository studentRepository) {
+    public EventServiceImpl(EventRepository eventRepository, StudentClubRepository studentClubRepository, StudentRepository studentRepository, EventRegistrationRepository eventRegistrationRepository) {
         this.eventRepository = eventRepository;
         this.studentClubRepository = studentClubRepository;
         this.studentRepository = studentRepository;
+        this.eventRegistrationRepository = eventRegistrationRepository;
     }
 
     @Override
@@ -99,7 +104,7 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public Slice<Event> getAllClubEvents(String clubId, int pageNumber, int pageSize) {
-        try{
+        try {
             Pageable pageable = PageRequest.of(pageNumber, pageSize);
             // find the club
             var club = studentClubRepository.findById(clubId).orElseThrow(() -> new RuntimeException("Club not found with id: " + clubId));
@@ -115,35 +120,63 @@ public class EventServiceImpl implements EventService {
         var event = eventRepository.findById(eventId).orElseThrow(() -> new RuntimeException("Event not found with id: " + eventId));
 
         // check if the event is full
-        if(event.getCapacity() == event.getCurrentAttendees()){
+        if (event.getCapacity() == event.getCurrentAttendees()) {
             throw new RuntimeException("Event is full");
 
         } else {
             // find the student
             var student = studentRepository.findById(studentId).orElseThrow(() -> new RuntimeException("Student not found with id: " + studentId));
+            // create the registration
+            var registration = new EventRegistration();
 
+            registration.setEvent(event);
+            registration.setStudent(student);
+            registration.setRegistrationTime(Instant.now());
+            registration.setStatus(EventRegistrationStatus.REGISTERED);
 
+            // save the registration on the database
+            var savedRegistration = eventRegistrationRepository.save(registration);
 
+            // increment attendee number by one
+            event.setCurrentAttendees(event.getCurrentAttendees() + 1);
+            eventRepository.save(event);
+
+            return savedRegistration;
         }
 
     }
 
     @Override
     public EventRegistration cancelEventRegistration(String eventRegistrationId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'cancelEventRegistration'");
+        // find the event registration
+        var eventRegistration = eventRegistrationRepository.findById(eventRegistrationId).orElseThrow(() -> new RuntimeException("Event registration not found"));
+
+        // update the event registration status to cancelled
+        eventRegistration.setStatus(EventRegistrationStatus.CANCELLED);
+        return eventRegistrationRepository.save(eventRegistration);
     }
 
     @Override
     public EventRegistration markAttendance(String eventRegistrationId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'markAttendance'");
+       // find the event registration
+        var eventRegistration = eventRegistrationRepository.findById(eventRegistrationId).orElseThrow(() -> new RuntimeException("Event registration not found"));
+
+        // update the event registration status to attended
+        eventRegistration.setStatus(EventRegistrationStatus.ATTENDED);
+        return eventRegistrationRepository.save(eventRegistration);
     }
 
     @Override
     public Slice<EventRegistration> getEventAttendanceList(String eventId, int pageNumber, int pageSize) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getEventAttendanceList'");
+       // set the pageable object
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+
+        // find the event
+        var event = eventRepository.findById(eventId).orElseThrow(() -> new RuntimeException("Event not found with id: " + eventId));
+
+        // get the rows where the event registration status is attended
+        return eventRegistrationRepository.findByStatus(EventRegistrationStatus.ATTENDED, pageable);
+
     }
 
 }
