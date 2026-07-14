a351994fb705cedc19861a625192d5d11bd0fd9a
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2026-07-14 11:24:58 +0300

    fix: use JPA Id annotation in EventRegistration

--- campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/event_module/models/EventRegistration.java
+++ campusphere-backend/src/main/java/com/sudobuild/campusphere_backend/event_module/models/EventRegistration.java
@@ -2,8 +2,6 @@ package com.sudobuild.campusphere_backend.event_module.models;
 
 import java.time.Instant;
 
-import org.springframework.data.annotation.Id;
-
 import com.sudobuild.campusphere_backend.event_module.enums.EventRegistrationStatus;
 import com.sudobuild.campusphere_backend.student_module.models.Student;
 
@@ -12,6 +10,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
